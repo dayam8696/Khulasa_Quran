a30363204cf6e80b1de196123537b5af799862c1
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2023-12-26 12:25:04 +0530

    added splash screen

---
+++ app/src/main/java/com/example/khulasa_quran/Fragments/ParaIndex.kt
@@ -0,0 +1,25 @@
+package com.example.khulasa_quran.Fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.khulasa_quran.databinding.ParaIndexBinding
+
+class ParaIndex:BaseFragment() {
+    private val binding by lazy { ParaIndexBinding.inflate(layoutInflater) }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
