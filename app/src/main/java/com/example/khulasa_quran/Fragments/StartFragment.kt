834bc46754050c6eaa73a96ff61bf34b777a0d82
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-01-15 14:24:39 +0530

    added next screen

---
+++ app/src/main/java/com/example/khulasa_quran/Fragments/StartFragment.kt
@@ -0,0 +1,23 @@
+package com.example.khulasa_quran.Fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.khulasa_quran.databinding.StartFragmentBinding
+
+class StartFragment:BaseFragment() {
+ private val binding by lazy { StartFragmentBinding.inflate(layoutInflater) }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
